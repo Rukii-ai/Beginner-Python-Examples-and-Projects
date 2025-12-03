c54a2ee3d4684e46f649fa1c587c5fd42e1d7ef6
Aguele Stephen <saguele.2302241@stu.cu.edu.ng>
2025-12-03 15:39:04 +0100

    update

---
+++ Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Exercise 2-12_Zen of Python.py
@@ -0,0 +1,31 @@
+import this
+
+"""
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly
+
+Simple is better than complex.
+If you have a choice between a simple and a complex
+ solution, and both work, use the simple solution. Your code
+ will be easier to maintain, and it will be easier for you and
+ others to build on that code later on.
+
+Complex is better than complicated.
+Readability counts.
+
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.  
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea. 
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+As you continue to the next chapter and start digging into
+ more involved topics, try to keep this philosophy of
+ simplicity and clarity in mind. Experienced programmers will
+ respect your code more and will be happy to give you
+ feedback and collaborate with you on interesting projects
+"""
--- Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Exercise 2.10.py
+++ Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Exercise 2.10.py
@@ -3,3 +3,17 @@ fav_num = 4
 message = f"My favorite number is {fav_num}."
 
 print(message)
+
+"""
+Skilled programmers expect to see
+ comments in code, so it’s best to start adding descriptive
+ comments to your programs now. Writing clear, concise
+ comments in your code is one of the most beneficial habits
+ you can form as a new programmer.
+ When you’re deciding whether to write a comment, ask
+ yourself if you had to consider several approaches before
+ coming up with a reasonable way to make something work;
+ if so, write a comment about your solution. It’s much easier
+ to delete extra comments later than to go back and write
+ comments for a sparsely commented program.
+"""
