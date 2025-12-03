b003883bf09615e2a3ce6dee2d73316382d3f34a
Aguele Stephen <saguele.2302241@stu.cu.edu.ng>
2025-12-03 15:21:41 +0100

    update

---
+++ Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Exercise 2.10.py
@@ -0,0 +1,5 @@
+fav_num = 4
+
+message = f"My favorite number is {fav_num}."
+
+print(message)
--- Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Numbers.py
+++ Python Crash course by Eric Matthes/Chapter 2_Variables and Simple Data Types/Numbers.py
@@ -1,6 +1,13 @@
-"""
-3^5 is not equal to 3**5.
-3**5 is exponential in python
-3^5 is bitwise XOR in python and outputs 6
+x, y, z = 0, 0, 0
+print(f"{x} \n{y} \n{z}\n")
 
 """
+A constant is a variable whose value stays the same
+ throughout the life of a program. Python doesn’t have built
+in constant types, but Python programmers use all capital
+ letters to indicate a variable should be treated as a constant
+ and never be changed
+"""
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
