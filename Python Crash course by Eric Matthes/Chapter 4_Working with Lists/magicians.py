179548907ce806658855d2c825a6087a199d3d05
Aguele Stephen <saguele.2302241@stu.cu.edu.ng>
2025-12-05 10:55:24 +0100

    update

---
+++ Python Crash course by Eric Matthes/Chapter 4_Working with Lists/magicians.py
@@ -0,0 +1,11 @@
+"""
+You’ll often want to run through all entries in a list,
+ performing the same task with each item. 
+ When you want to do the same action
+ with every item in a list, you can use Python’s for loop.
+"""
+
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:
+    print(magician.title())
