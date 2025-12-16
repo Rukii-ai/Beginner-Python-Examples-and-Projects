252883e588d70aa5e2d2b5b89ceb583cf8cf5d5c
Aguele Stephen <saguele.2302241@stu.cu.edu.ng>
2025-12-16 12:57:46 +0100

    update

---
+++ Python Crash course by Eric Matthes/Chapter 4_Working with Lists/Exercise 4.1.py
@@ -0,0 +1,11 @@
+pizzas = ['pepperoni', 'margherita', 'bbq chicken']
+
+for pizza in pizzas:
+    print(pizza)
+
+print("")
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("\nI really love pizza!")
---
+++ Python Crash course by Eric Matthes/Chapter 4_Working with Lists/Exercise 4.2.py
@@ -0,0 +1,11 @@
+tailed_animals = ['dog', 'cat', 'rabbit']
+
+for animal in tailed_animals:
+    print(animal)
+
+print("")
+
+for animal in tailed_animals:
+    print(f"A {animal} would make a great pet.")
+
+print(f"\nA {tailed_animals[0].title()}, {tailed_animals[1].title()} and {tailed_animals[2].title()} all have a tail. Any of these animals would make a great pet!")
