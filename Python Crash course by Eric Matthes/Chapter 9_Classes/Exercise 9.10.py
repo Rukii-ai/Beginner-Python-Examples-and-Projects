1ca6679bbe672f405f25429ca102c0709aa60220
Aguele Stephen <saguele.2302241@stu.cu.edu.ng>
2026-01-24 13:28:06 +0100

    update

--- Python Crash course by Eric Matthes/Chapter 9_Classes/Exercise 9.10.py
+++ Python Crash course by Eric Matthes/Chapter 9_Classes/Exercise 9.10.py
@@ -1,1 +1,6 @@
-import R
+from Restaurant_module import Restaurant as RS
+
+fav_restaurant = RS("The Culinary Spot", "fusion cuisine")
+print(fav_restaurant.restaurant_name)
+
+fav_restaurant.open_restaurant()
